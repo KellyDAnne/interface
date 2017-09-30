b93552ebc1ca1ab609b1bcd3191eb8df32f42100
KellyDAnne <kelly.anne@utt.fr>
2017-09-30 12:42:29 +0200

    first commit

---
+++ src/main/scala/fr/kelly/corp/backend.scala
@@ -0,0 +1,29 @@
+package fr.kelly.corp
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives.{complete, get, path}
+import akka.stream.ActorMaterializer
+
+import scala.io.StdIn
+
+object backend extends App{
+  implicit val system = ActorSystem("my-system")
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+
+  val route =
+    path("") {
+      get {
+        complete("I am a cat")
+      }
+    }
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 80)
+
+  println(s"Server online at http://localhost:8085/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
+}
